4862dc0dd984fce860cc4f38146aaa43855271cb
Sebastian Gutsche <gutsche@momo.math.rwth-aachen.de>
2016-01-04 14:01:40 +0100

    First version of new kernel.

--- wrapper-kernel/jupyter_singular_wrapper/kernel.py
+++ wrapper-kernel/jupyter_singular_wrapper/kernel.py
@@ -1,6 +1,4 @@
 from ipykernel.kernelbase import Kernel
-import pexpect
-from pexpect import replwrap, EOF, which
 
 from subprocess import check_output
 from os import unlink, path
@@ -11,7 +9,9 @@ import re
 import signal
 import urllib
 
-__version__ = '0.3'
+from SingularPython import RunSingularCommand
+
+__version__ = '0.4'
 
 version_pat = re.compile(r'version (\d+(\.\d+)+)')
 
@@ -48,12 +48,7 @@ class SingularKernel(Kernel):
 
     def _start_singular(self):
         sig = signal.signal(signal.SIGINT, signal.SIG_DFL)
-        try:
-            singular_run_command = pexpect.which( "Singular" )
-            self.singularwrapper = pexpect.spawnu( singular_run_command + " -q" )
-            self.singularwrapper.expect( "> " )
-        finally:
-            signal.signal(signal.SIGINT, sig)
+        signal.signal(signal.SIGINT, sig)
 
     def do_execute(self, code, silent, store_history=True,
                    user_expressions=None, allow_stdin=False):
@@ -62,82 +57,72 @@ class SingularKernel(Kernel):
                     'payload': [], 'user_expressions': {}}
 
         interrupted = False
-        try:
-            code_stripped = code.rstrip()
-            self.singularwrapper.sendline( code_stripped + "//singular_jupyter_scan_comment" )
-            self.singularwrapper.expect( [ "//singular_jupyter_scan_comment\r\n" ] )
-            self.singularwrapper.expect( "> " )
-            output = self.singularwrapper.before
-        except KeyboardInterrupt:
-            self.singularwrapper.child.sendintr()
-            interrupted = True
-            self.singularwrapper.expect( "> " )
-            output = self.singularwrapper.before
-        except EOF:
-            output = self.singularwrapper.before + 'Restarting singular'
-            self._start_singular()
-
-        if not silent:
-            if output.strip() != "":
-                stream_content = {'execution_count': self.execution_count, 'data': { 'text/plain': output } }
-                self.send_response(self.iopub_socket, 'execute_result', stream_content)
-
-        if interrupted:
-            return {'status': 'abort', 'execution_count': self.execution_count}
-
-        try:
-            exitcode = 0
-        except Exception:
-            exitcode = 1
-
-        if exitcode:
-            return {'status': 'error', 'execution_count': self.execution_count,
-                    'ename': '', 'evalue': str(exitcode), 'traceback': []}
-        else:
+        
+        code_stripped = code.rstrip()
+        output = RunSingularCommand( code )
+        
+        output_error = output[ 0 ]
+        output_string = output[ 1 ]
+        
+        if not output_error:
+            if not silent:
+                if output_string.strip() != "":
+                    stream_content = {'execution_count': self.execution_count, 'data': { 'text/plain': output_string } }
+                    self.send_response( self.iopub_socket, 'execute_result', stream_content )
+            
+            
             return {'status': 'ok', 'execution_count': self.execution_count,
                     'payload': [], 'user_expressions': {}}
-
-    # This is a rather poor completion at the moment
-    def do_complete(self, code, cursor_pos):
-        code = code[:cursor_pos]
-        default = {'matches': [], 'cursor_start': 0,
-                   'cursor_end': cursor_pos, 'metadata': dict(),
-                   'status': 'ok'}
-
-        if not code or code[-1] == ' ':
-            return default
-
-        matches = []
-        token = code.encode( "utf-8" )
-        start = cursor_pos - len(token)
-
-        # complete bound global variables
         
-        scan_string = "// " + code
+        elif output_error:
+            stream_content = { 'execution_count': self.execution_count, 'data': { 'text/plain': output_string } }
+            self.send_response( self.iopub_socket, 'error', stream_content )
+            return {'status': 'error', 'execution_count': self.execution_count,
+                    'ename': '', 'evalue': '1', 'traceback': []}
         
-        self.singularwrapper.send( scan_string )
-        self.singularwrapper.send( "\t\t\t" )
-        out_num = self.singularwrapper.expect( [ "Display", "> " ] )
-        
-        if out_num == 0:
-            output_list = self.singularwrapper.before[3:]
-            self.singularwrapper.send( "n\r\n" )
-        else:
-            output_list = self.singularwrapper.before
-        
-        matches.extend(output_list.split())
-        
-        self.singularwrapper.sendline( "" )
-        self.singularwrapper.sendline( ";//singular_jupyter_scan_comment" )
-        self.singularwrapper.expect( [ "//singular_jupyter_scan_comment" ] )
-        self.singularwrapper.expect( [ "> " ] )
-        
-        matches = [m for m in matches if m.isalnum() ]
+    
 
-        if not matches:
-            return default
+    ## This is a rather poor completion at the moment
+    #def do_complete(self, code, cursor_pos):
+        #code = code[:cursor_pos]
+        #default = {'matches': [], 'cursor_start': 0,
+                   #'cursor_end': cursor_pos, 'metadata': dict(),
+                   #'status': 'ok'}
 
-        return {'matches': sorted(matches), 'cursor_start': start,
-                'cursor_end': cursor_pos, 'metadata': dict(),
-                'status': 'ok'}
+        #if not code or code[-1] == ' ':
+            #return default
+
+        #matches = []
+        #token = code.encode( "utf-8" )
+        #start = cursor_pos - len(token)
+
+        ## complete bound global variables
+        
+        #scan_string = "// " + code
+        
+        #self.singularwrapper.send( scan_string )
+        #self.singularwrapper.send( "\t\t\t" )
+        #out_num = self.singularwrapper.expect( [ "Display", "> " ] )
+        
+        #if out_num == 0:
+            #output_list = self.singularwrapper.before[3:]
+            #self.singularwrapper.send( "n\r\n" )
+        #else:
+            #output_list = self.singularwrapper.before
+        
+        #matches.extend(output_list.split())
+        
+        #self.singularwrapper.sendline( "" )
+        #self.singularwrapper.sendline( ";//singular_jupyter_scan_comment" )
+        #self.singularwrapper.expect( [ "//singular_jupyter_scan_comment" ] )
+        #self.singularwrapper.expect( [ "> " ] )
+        
+        #matches = [m for m in matches if m.isalnum() ]
+
+        #if not matches:
+            #return default
+
+        #return {'matches': sorted(matches), 'cursor_start': start,
+                #'cursor_end': cursor_pos, 'metadata': dict(),
+                #'status': 'ok'}
 
